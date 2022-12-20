256c232c418c7042d6075665952fa27c89c87f61
Josea M <92557972+josea17m@users.noreply.github.com>
2022-12-20 14:49:52 -0500

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+      <main className="main-container">
+        <div className="main">
+            <h1>Hola Mundo</h1>
+        </div>
+      </main>
+    );
+  }
+  
+  export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <ul className="navbar">
+        <li>Dia</li>
+        <li>Mes</li>
+        <li>Año</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
